309ad4e5d017bac2d733d61782a7acefeda6ea73
ejioforJs <84478545+ejioforJs@users.noreply.github.com>
2024-07-10 07:54:28 +0100

    Implement the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+    const loggedIn = {firstName: 'James', lastName: 'Solomon', email: 'ejioforjames12@gmail.com'}
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+          type='greeting'
+          title='welcome'
+          user={loggedIn.firstName }
+          subtext='Access and manage your account and transactions efficiently'
+          />
+          <TotalBalanceBox
+          accounts= {[]}
+          totalBanks= {1}
+          totalCurrentBalance={1250.35}
+           />
+        </header>
+        RECENT TRANSACTIONS
+      </div>
+      <RightSidebar user={loggedIn} transactions={[]} banks={[{currentBalance: 123.50}, {currentBalance: 523.50}]} />
+    </section>
+  );
+};
 
-export default Home
+export default Home;
