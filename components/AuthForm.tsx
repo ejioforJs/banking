7c7ce63a3e11d45786f6306a859c56a339f2013f
ejioforJs <84478545+ejioforJs@users.noreply.github.com>
2024-07-13 09:07:36 +0100

    implement the complete layout

---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthForm = ({ type }: { type: string }) => {
+  return (
+    <section className="auth-form">
+      <header className="flex flex-col gap-5 md:gap-8"></header>
+    </section>
+  );
+};
+
+export default AuthForm;
